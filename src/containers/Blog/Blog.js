f8b3fcbf5ba266b01353052c8b5ba12c0479af0d
acparkgh <acpark123@gmail.com>
2021-02-08 23:52:35 -0500

    used axios to fetch data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,15 +4,35 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from "axios";
 
 class Blog extends Component {
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount () {
+    // fetch ('https://jsonplaceholder.typicode.com/posts')
+        // .then( response => response.json() )
+        // .then( data => this.setState({ posts: data }));
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+        // .then(response => { console.log(response.data) })
+        .then(response => { 
+          this.setState({ posts: response.data }) 
+        });
+    }
+
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post title={post.title} 
+                         key={post.id} 
+                   />
+        })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                {posts}
                 </section>
                 <section>
                     <FullPost />
