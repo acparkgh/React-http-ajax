02ef0a3637fd0e61456de8105e75fc71d07bd2e1
acparkgh <acpark123@gmail.com>
2021-02-10 01:26:23 -0500

    added axis global default URL

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -18,7 +18,7 @@ class NewPost extends Component {
           auther: this.state.author,
        };
         
-       axios.post('https://jsonplaceholder.typicode.com/posts', postData)
+       axios.post('/posts', postData)
            .then(response => console.log(response))
     }
 
