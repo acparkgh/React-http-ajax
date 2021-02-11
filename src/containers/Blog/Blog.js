33505b6772c920b04de9ea4519ca35929e9a3f27
acparkgh <acpark123@gmail.com>
2021-02-11 16:05:40 -0500

    rearranged folders and added BrowserRouter to add.js

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+// import Post from '../../components/Post/Post';
+// import FullPost from '../FullPost/FullPost';
+// import NewPost from '../NewPost/NewPost';
 import './Blog.css';
 // import axios from "axios";
 import axios from '../../axios'
